fe932028f304a731139f6559db38a460d8ce4e47
Gleb <gleb.pastushenkko@gmail.com>
2017-02-11 23:41:29 +0200

    finish

--- source-js/main.js
+++ source-js/main.js
@@ -4,6 +4,8 @@ function onClickCreator() {
     $('.tabs-container .tabs li').attr('onClick', 'tabActivator(this)');
     $('.tabs-container .popup-placeholder').attr('onClick', 'popupActivator(this)');
     $('.tabs-container .popup').attr('onClick', 'popupDeActivator(this)');
+    $('.accordion-container .accordion-item .item-title').attr('onClick', 'accordionActivator(this)');
+    $('.main-menu li').attr('onClick', 'moveToSection(this)');
 }
 
 function tabActivator(caller) {
@@ -25,5 +27,36 @@ function popupActivator(caller) {
 
 function popupDeActivator(caller) {
     $(caller).parent().parent().removeClass('active');
+    $('.body-disable').css('display', 'none');
+    $('body').css('disabled');
+}
+
+function mainMenuToggle() {
+
+    if ($('#main-menu').attr('data-toggle') == 'hidden') {
+        $('#main-menu').css('visibility', 'visible');
+        $('#main-menu').attr('data-toggle', 'visible');
+    } else {
+        $('#main-menu').css('visibility', 'hidden');
+        $('#main-menu').attr('data-toggle', 'hidden');
+    }
+}
+
+function accordionActivator(caller) {
+    $(caller).parent().siblings('.active').removeClass('active');
+    $(caller).parent().addClass('active');
+}
+
+function moveToSection() {
+    $('body').animate('scrollTop', $(caller).href.offsetTop);
+}
+
+function modalShow() {
+    $('.modal-form').addClass('active');
+    $('.body-disable').css('display', 'block');
+}
+
+function modalHide() {
+    $('.modal-form').removeClass('active');
     $('.body-disable').css('display', 'none');
 }
